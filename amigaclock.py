af6fcc4dd00dfca643118f35089cbbfcc71c1df6
Martin C. Doege <mdoege@compuserve.com>
2020-06-03 23:04:11 +0200

    simplify

--- amigaclock.py
+++ amigaclock.py
@@ -16,7 +16,7 @@ ORANGE = 255, 136, 0
 class AmigaClock:
 	def __init__(s):
 		pygame.init()
-		s.res = 400, 400
+		s.res = 350, 400
 		s.screen = pygame.display.set_mode(s.res, pygame.RESIZABLE)
 		pygame.display.set_caption('AmigaClock')
 		s.screen.fill(BLUE)
@@ -42,6 +42,19 @@ class AmigaClock:
 		z = min(s.res[0], s.res[1])
 		return int(.95 * z/2)
 
+	def diam(s, x, r1, r2, r3, dx):
+		x1, y1 = r1*s.clocksize()*sin(x*pi/180), r1*s.clocksize()*cos(x*pi/180)
+		x2, y2 = r2*s.clocksize()*sin((x+dx)*pi/180), r2*s.clocksize()*cos((x+dx)*pi/180)
+		x3, y3 = r3*s.clocksize()*sin(x*pi/180), r3*s.clocksize()*cos(x*pi/180)
+		x4, y4 = r2*s.clocksize()*sin((x-dx)*pi/180), r2*s.clocksize()*cos((x-dx)*pi/180)
+		p = [
+			(int(x1+s.res[0]//2), int(-y1+s.res[1]//2)),
+			(int(x2+s.res[0]//2), int(-y2+s.res[1]//2)),
+			(int(x3+s.res[0]//2), int(-y3+s.res[1]//2)),
+			(int(x4+s.res[0]//2), int(-y4+s.res[1]//2)),
+		]
+		pygame.draw.polygon(s.screen, BLACK, p)
+
 	def update(s):
 		s.screen.fill(BLUE)
 		pygame.draw.circle(s.screen, BLACK, (s.res[0]//2, s.res[1]//2), s.clocksize())
@@ -52,46 +65,17 @@ class AmigaClock:
 			pygame.draw.line(s.screen, BLACK,
 				(int(x1+s.res[0]//2), int(y1+s.res[1]//2)),
 				(int(x2+s.res[0]//2), int(y2+s.res[1]//2)), 2)
-		for x in range(0, 60, 5):
-			x1, y1 = .95*s.clocksize()*sin(6*x*pi/180), .95*s.clocksize()*cos(6*x*pi/180)
-			x2, y2 = .88*s.clocksize()*sin(6*(.5+x)*pi/180), .88*s.clocksize()*cos(6*(.5+x)*pi/180)
-			x3, y3 = .81*s.clocksize()*sin(6*x*pi/180), .81*s.clocksize()*cos(6*x*pi/180)
-			x4, y4 = .88*s.clocksize()*sin(6*(x-.5)*pi/180), .88*s.clocksize()*cos(6*(x-.5)*pi/180)
-			p = [
-				(int(x1+s.res[0]//2), int(y1+s.res[1]//2)),
-				(int(x2+s.res[0]//2), int(y2+s.res[1]//2)),
-				(int(x3+s.res[0]//2), int(y3+s.res[1]//2)),
-				(int(x4+s.res[0]//2), int(y4+s.res[1]//2)),
-			]
-			pygame.draw.polygon(s.screen, BLACK, p)
+		for x in range(0, 360, 30):
+			s.diam(x, .95, .88, .81, 3)
+
 		# hour hand
 		now = datetime.datetime.now()
 		x = 30*((now.hour - 12) % 12) + now.minute/2
-		x1, y1 = .55*s.clocksize()*sin(x*pi/180), .55*s.clocksize()*cos(x*pi/180)
-		x2, y2 = .42*s.clocksize()*sin((6+x)*pi/180), .42*s.clocksize()*cos((6+x)*pi/180)
-		x3, y3 = 0, 0
-		x4, y4 = .42*s.clocksize()*sin((x-6)*pi/180), .42*s.clocksize()*cos((x-6)*pi/180)
-		p = [
-			(int(x1+s.res[0]//2), int(-y1+s.res[1]//2)),
-			(int(x2+s.res[0]//2), int(-y2+s.res[1]//2)),
-			(int(x3+s.res[0]//2), int(-y3+s.res[1]//2)),
-			(int(x4+s.res[0]//2), int(-y4+s.res[1]//2)),
-		]
-		pygame.draw.polygon(s.screen, BLACK, p)
+		s.diam(x, .55, .42, 0, 6)
 
 		# minute hand
 		x = 6*now.minute + now.second/10
-		x1, y1 = .8*s.clocksize()*sin(x*pi/180), .8*s.clocksize()*cos(x*pi/180)
-		x2, y2 = .68*s.clocksize()*sin((3+x)*pi/180), .68*s.clocksize()*cos((3+x)*pi/180)
-		x3, y3 = 0, 0
-		x4, y4 = .68*s.clocksize()*sin((x-3)*pi/180), .68*s.clocksize()*cos((x-3)*pi/180)
-		p = [
-			(int(x1+s.res[0]//2), int(-y1+s.res[1]//2)),
-			(int(x2+s.res[0]//2), int(-y2+s.res[1]//2)),
-			(int(x3+s.res[0]//2), int(-y3+s.res[1]//2)),
-			(int(x4+s.res[0]//2), int(-y4+s.res[1]//2)),
-		]
-		pygame.draw.polygon(s.screen, BLACK, p)
+		s.diam(x, .8, .68, 0, 3)
 
 		# second hand
 		x = 6*now.second
