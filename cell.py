35d9f572c921d2f80f41fa915554f9caea5a5f1e
Martin C. Doege <mdoege@compuserve.com>
2020-06-16 15:55:39 +0200

    add cellular automaton

---
+++ cell.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# Cellular automaton
+
+import pygame, random
+
+def getcol():
+    return [85 * random.randint(0, 3) for q in (1, 2, 3)]
+
+COL = getcol(), getcol()
+FPS = 60
+RULES = 18, 22, 26, 30, 41, 45, 54, 60, 90, 102, 105, 106, 110, 122, 126, 146, 150, 154, 182
+RES = 300, 500
+NCELL = RES[0]
+
+class Cell:
+    def __init__(self):
+        pygame.init()
+        self.res = RES
+        self.screen = pygame.display.set_mode(self.res, pygame.RESIZABLE)
+        pygame.display.set_caption('cell')
+        self.clock = pygame.time.Clock()
+        self.cell = [random.randint(0, 1) for q in range(2 + NCELL)]
+        self.rule = random.choice(RULES)
+        self.gen = 1
+        self.line = 0
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: self.running = False
+            if event.type == pygame.VIDEORESIZE:
+                self.res = event.w, event.h
+                self.screen = pygame.display.set_mode(self.res, pygame.RESIZABLE)
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+        pygame.quit()
+
+    def newgen(self):
+        self.gen += 1
+        cell2 = [0 for q in range(2 + NCELL)]
+        for x in range(NCELL):
+            s = 2 ** (4 * self.cell[x] + 2 * self.cell[x + 1] + self.cell[x + 2])
+            if s & self.rule:
+                cell2[x + 1] = 1
+        # make cyclic
+        cell2[0] = self.cell[-2]
+        cell2[-1] = self.cell[1]
+        self.cell = cell2
+
+    def update(self):
+        self.line += 1
+        #print(self.line, self.gen)
+        self.screen.scroll(dy = -1)
+        pygame.draw.line(self.screen, (0, 0, 0),
+            (0, self.res[1]-1),
+            (self.res[0]-1, self.res[1]-1), 1)
+        scrollfac = max(1, int(self.res[0] / RES[0]))
+        if self.line % scrollfac == 0:
+            self.newgen()
+        for x in range(self.res[0]):
+            xp = int(x / self.res[0] * NCELL)
+            pygame.draw.line(self.screen, COL[self.cell[xp + 1]],
+                (x, self.res[1]-1),
+                (x, self.res[1]-1), 1)
+        pygame.display.set_caption('cell (rule %u, generation %u)' % (self.rule, self.gen))
+        
+        pygame.display.flip()
+
+c = Cell()
+c.run()
+
